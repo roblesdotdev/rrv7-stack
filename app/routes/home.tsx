2b9ea5bd9c00181075b91d63e2e0f3a08a98269c
roblesdotdev <httpsnip@gmail.com>
2024-11-25 22:39:41 -0300

    setup biome & vscode settings

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,16 @@
-import type { Route } from "./+types/home";
+import type { Route } from './+types/home';
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function meta({ error }: Route.MetaArgs) {
+	return [
+		{ title: error ? 'Error' : 'New React Router App' },
+		{ name: 'description', content: 'Welcome to React Router!' },
+	];
 }
 
 export default function Home() {
-  return (
-    <div className="min-h-screen grid place-items-center">
-      <h1 className="text-xl">Working</h1>
-    </div>
-  );
+	return (
+		<div className="min-h-screen grid place-items-center">
+			<h1 className="text-xl">Working</h1>
+		</div>
+	);
 }
